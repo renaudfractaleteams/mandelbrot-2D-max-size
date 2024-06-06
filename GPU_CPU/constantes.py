c3a443a6d6ac47233b46e1993de661c7272ee3e4
Renaud HENRY <git@rhenry.fr>
2024-06-06 18:08:14 +0200

    Update constantes.py

--- GPU_CPU/constantes.py
+++ GPU_CPU/constantes.py
@@ -5,8 +5,8 @@ NB_TUILES = 512
 
 
 
-PATH_BASE_G = "./web/pan/mandelbrot_G2_files"
-PATH_BASE_BW = "./web/pan/mandelbrot_BW2_files"
+PATH_BASE_G = "./web/pan/mandelbrot_G_files"
+PATH_BASE_BW = "./web/pan/mandelbrot_BW_files"
 """
 PATH_BASE_G = "./web/pan/mandelbrot_G_test_files"
 
