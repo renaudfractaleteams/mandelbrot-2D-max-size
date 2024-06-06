77dc042fc92baf3c51523813e3fe70280ea6bde2
Renaud HENRY <git@rhenry.fr>
2024-06-06 18:03:07 +0200

    Configuration

--- GPU_CPU/constantes.py
+++ GPU_CPU/constantes.py
@@ -4,10 +4,11 @@ BOOL_INVERSE = False
 NB_TUILES = 512
 
 
-"""
+
 PATH_BASE_G = "./web/pan/mandelbrot_G2_files"
 PATH_BASE_BW = "./web/pan/mandelbrot_BW2_files"
 """
 PATH_BASE_G = "./web/pan/mandelbrot_G_test_files"
 
 PATH_BASE_BW = "./web/pan/mandelbrot_BW_test_files"
+"""
--- GPU_CPU/main_CPU_GPU.py
+++ GPU_CPU/main_CPU_GPU.py
@@ -9,4 +9,4 @@ import constantes #import BOOL_INVERSE, NB_TUILES, PATH_BASE_G, PATH_BASE_BW
 from lib_CPU import make_tuiles
 
 #make_sub_tuile_base()
-make_tuiles(engine=constantesg.CUDA_LIB.RUN_CUDA,UseCoef2=False, UseEngine=True,constantes=constantes)
+make_tuiles(engine=constantesg.CUDA_LIB.RUN_CUDA,UseCoef2=True, UseEngine=False,constantes=constantes)
